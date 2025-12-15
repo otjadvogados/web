4e98787a7102c33b212ed1a249bb030a4ff38f25
Higor <codehs07@gmail.com>
2025-12-15 07:00:12 -0300

    Reset initial values for email and password fields in AuthLogin component to empty strings for improved user experience during login.

--- src/sections/auth/jwt/AuthLogin.tsx
+++ src/sections/auth/jwt/AuthLogin.tsx
@@ -50,8 +50,8 @@ export default function AuthLogin({ isDemo = false }: { isDemo?: boolean }) {
     <>
       <Formik
         initialValues={{
-          email: 'hms.swiftsoft@gmail.com',
-          password: 'Admin@123456!',
+          email: '',
+          password: '',
           submit: null
         }}
         validationSchema={Yup.object().shape({
